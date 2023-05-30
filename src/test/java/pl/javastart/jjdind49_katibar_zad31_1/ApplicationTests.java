ed36ed5a886e4623f1cf1f28fd1f11dba2702004
KatiBar <katarzyna.kinga.baranska@gmail.com>
2023-05-30 23:17:40 +0200

    Init commit

---
+++ src/test/java/pl/javastart/jjdind49_katibar_zad31_1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.jjdind49_katibar_zad31_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
